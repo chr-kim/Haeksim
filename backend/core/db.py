f92ecd7d76e3883fd86fdabcf91098693089dfa1
chr-kim <chriankim@gmail.com>
2025-09-16 10:32:28 +0900

    Delete backend directory

--- backend/core/db.py
+++
@@ -1,8 +0,0 @@
-# backend/core/db.py
-from sqlalchemy import create_engine, MetaData
-import databases
-from .config import DATABASE_URL
-
-database = databases.Database(DATABASE_URL)
-metadata = MetaData()
-engine = create_engine(DATABASE_URL)
